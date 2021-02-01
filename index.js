f554145b4f1b6f9f1d1f20a5b955ce0e944a0efb
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2021-02-01 13:09:36 +0300

    new kata

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-function getDecimal(n) {
-  let num = n < 0 ? -n : n
-  let num2 = Math.floor(num)
-  return num - num2; // fix me 
+function beggars(values, n) {
+  //your code here
+  const newArray = Array(n).fill(0);
+  if (n === 0) return [];
+  let i = 0;
+  do {
+    for (let index = 0; index < n; index++) {
+      newArray[index] += values[i] ? values[i] : 0;
+      i++;
+    }
+  } while (i < values.length);
+
+  return newArray;
 }
 
-console.log(getDecimal(10), 0);
-
-console.log(getDecimal(-1.2), 0.2);
-
-console.log(getDecimal(4.5), 0.5);
+console.log(beggars([1, 2, 3, 4, 5], 1), [15]);
+console.log(beggars([1, 2, 3, 4, 5], 2), [9, 6]);
+console.log(beggars([1, 2, 3, 4, 5], 3), [5, 7, 3]);
+console.log(beggars([1, 2, 3, 4, 5], 6), [1, 2, 3, 4, 5, 0]);
+console.log(beggars([1, 2, 3, 4, 5], 0), []);
