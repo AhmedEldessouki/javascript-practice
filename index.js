890381c4434d30ccd04ef7f86156326c9c480919
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2021-02-10 01:02:49 +0300

    another Katas

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function beggars(str = '') {
-  return str.replace(/[A-Z]/g, (s) => ` ${s}`)
+function moveZeros(arr = '') {
+  function twistIt(str = '') {
+    return str.split('').map((a, i, arr) => arr[arr.length - i - 1]).join('')
+  }
+  return arr.split(' ').map(item => twistIt(item)).join(' ')
 }
 
-console.log(beggars('mochaPad'))
-console.log(beggars('mochaPadPad'))
-console.log(beggars('mochaPadMac'))
+console.log(moveZeros("double  spaces"), "double  spaces", '==>', "elbuod  secaps")
+console.log(moveZeros("double  spaces"), "double  spaces", '==>', "elbuod  secaps")
+console.log(moveZeros("double  spaces"), "double  spaces", '==>', "elbuod  secaps")
+
+function manhattanDistance(arr0, arr1) {
+  function fn(params) {
+
+    return Math.abs(params)
+  }
+  return (fn(arr0[0] - arr1[0]) + fn(arr0[1] - arr1[1]))
+}
+
+console.log(manhattanDistance([1, 1], [1, 1]), '=> returns 0')
+console.log(manhattanDistance([5, 4], [3, 2]), '=> returns 4')
+console.log(manhattanDistance([1, 1], [0, 3]), '=> returns 3')
