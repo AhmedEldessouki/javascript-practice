a9394a5aed490fd8b78097a0ba11a5c898ffdd62
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2021-02-17 13:58:39 +0300

    Higher Order Function

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,25 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
-    'standard',
+    'airbnb-base',
     'prettier/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: [
-    '@typescript-eslint'
+    '@typescript-eslint',
   ],
   rules: {
-  }
-}
+    'no-console': 0,
+    'no-undef': 1,
+    'no-unused-vars': 1,
+    'no-return-assign': 0,
+    'no-param-reassign': 0,
+  },
+};
--- index.js
+++ index.js
@@ -1,19 +1,64 @@
+const chalk = require('chalk');
+const arr = [
+  ['1536', '1541'],
+  ['1542', '1548'],
+  ['1549', '1563'],
+  ['1564', '1565'],
+  ['1566', '1567'],
+  ['1568', '1600'],
+  ['1601', '1611', '1611'],
+  ['126523', '126524'],
+  ['126530', '126531'],
+  ['126535', '126536'],
+];
 
-function manhattanDistance(nFloors) {
-  const arr = []
-  if (nFloors < 0) return arr
-  for (let i = 0; i < nFloors; i++) {
-    let str = '*'
-    str = str.padStart(nFloors - i, ' ')
-    for (let x = 0; x < i; x++) {
-      str += '**'
-    }
-    str = str.padEnd(nFloors * 2 - 1, ' ')
-    arr.push(str)
+function flattening(thatArr) {
+  if (!Array.isArray(thatArr)) {
+    return thatArr;
   }
-  return arr
+  const x = thatArr.reduce((a, b) => a.concat(b));
+  return flattening(x);
 }
 
-console.log(manhattanDistance(1 + 1))
-console.log(manhattanDistance(4 + 2))
-console.log(manhattanDistance(1 + 3))
+console.log(flattening(arr));
+
+function yourOwnLoop(value, testFunction, updateFunction) {
+  return (bodyFunction) => {
+    let x;
+    for (let index = value; testFunction(index); index = updateFunction(index)) {
+      x = bodyFunction(index);
+      if (!x) {
+        return x
+      }
+    }
+    return x
+  };
+}
+const loopsTenTimesAndIncrementBy1 = yourOwnLoop(10, (i) => i < 20, (n) => {
+  return n + 1;
+});
+
+loopsTenTimesAndIncrementBy1((number) => {
+  return number;
+})
+
+// ======== Array.Every ========
+
+const arrayEvery = (arrr, predicate) => {
+  const arrayLooping = yourOwnLoop(0, (i, max = arrr.length) => i < max, (n) => {
+    return n + 1;
+  });
+  let result = arrayLooping((i, arrX = arr) => {
+    let x = predicate(arrX[i])
+    return x
+  })
+
+  return result
+
+}
+console.log(chalk.bgBlue(chalk.black('[Array.Every] ', arr.length, arr[0].length)))
+console.log(chalk.bgRedBright(chalk.black('[Array.Every]: Should Fail', arrayEvery(arr, item => item.length < 3))))
+console.log(chalk.bgGreenBright(chalk.black('[Array.Every]: Should Pass', arrayEvery(arr, item => item.length < 4))))
+console.log(chalk.bgRedBright(chalk.black('[Array.Every]: Should Fail', arr.every(item => item.length < 3))))
+console.log(chalk.bgGreenBright(chalk.black('[Array.Every]: Should Pass', arr.every(item => item.length < 4))))
+console.log(chalk.bgGreenBright(chalk.black('[Array.Some]: Should Pass', arr.some(item => item.length > 2))))
