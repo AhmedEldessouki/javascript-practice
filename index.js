0d9b022ae96be023c32a358a5a919ce656e8a064
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-06-18 23:56:40 +0300

    some updates and another kata

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-function expandedForm(num = 0) {
-  function returnZero(params, i, arr) {
-    if (i === arr.length - 1) {
-      return params;
-    }
-    return returnZero(params + "0", (i += 1), arr);
-  }
-  const arr = num
-    .toString()
-    .split("")
-    .map((item, i, arr) => {
-      if (parseInt(item) >= 1) {
-        return returnZero(item, i, arr);
-      }
-      return item;
-    });
+// ! An Awesome answer! NOT MINE
+// function solve(str = "") {
+//   let arr = [...str].filter((x) => x != " ");
+//   console.log(arr);
 
-  return arr.filter((a) => a > 0).join(" + ");
+//   return str.replace(/\S/g, (_) => {
+//     console.log(_);
+//     return arr.pop();
+//   });
+// }
+function solve(str = "") {
+  const spaceIndexes = [];
+  const newArr = str.split("");
+  newArr.forEach((item, i, arr) => {
+    if (item === " ") {
+      spaceIndexes.push(i);
+      arr.splice(i, 1);
+    }
+  });
+  newArr.reverse();
+  spaceIndexes.forEach((item, i) => {
+    newArr[item - 1] += " ";
+  });
+  return newArr.join("");
 }
-console.log(expandedForm(12)); // Should return '10 + 2'
-console.log(expandedForm(42)); // Should return '40 + 2'
-console.log(expandedForm(70304)); // Should return '70000 + 300 + 4'
-console.log(expandedForm(402070022)); // Should return '70000 + 300 + 4'
+console.log(solve("your code rocks")); // Should return  "skco redo cruoy"
+console.log(solve("codewars")); // Should return "srawedoc"
+console.log(
+  solve(
+    "o we pmpetva vwr bno kp d t j qe lb d ug ztjr rdqgjg gxwugtu zp d o djc lxbx dtqda b qc rm u q rkiehszc"
+  )
+);
