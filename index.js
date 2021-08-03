bab925f2c77e8aa1440adddb5ae06d4e7288ff95
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-08-03 18:10:17 +0300

    some logic question

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-// ? An array of numbers is given : [1,2,31,24, -67,5,3,555,79, -4,21,33,89, -90].
-// ? Find the maximum and minimum value of the array. Write all positive and even numbers into another array.
-// ? Output the results on the screen.
+// ? Write the function bigToSmall, which will take one argument arr (two-dimensional array).
+// ? Bring the two-dimensional array to one-dimensional. Sort the array in descending order.
+// ? Use the '>' character as a separator when converting an array to a string. For example:
+// ? bigToSmall([[1,2],[3,4],[5,6]]) "6>5>4>3>2>1"
+// ? bigToSmall([[1,3,5],[2,4,6]]) "6>5>4>3>2>1"
+// ? bigToSmall([[1,1],[1],[1,1]]) "1>1>1>1>1"
 
-function magic(param = []) {
-  const maxNumber = Math.max(...param)
-  const minNumber = Math.min(...param)
-
-  const positiveEven = []
-  param.forEach(num => {
-    if (num > 0 && num % 2) {
-      positiveEven.push(num)
+function bigToSmall(arr) {
+  var arr2 = []
+  for (var i = arr.length - 1; i >= 0; i--) {
+    for (var j = arr[i].length - 1; j >= 0; j--) {
+      arr2.push(arr[i][j])
     }
-  }, [])
-
-  return {maxNumber, minNumber, positiveEven}
+  }
+  return arr2.join('>')
 }
 
-console.log(magic([1, 2, 31, 24, -67, 5, 3, 555, 79, -4, 21, 33, 89, -90]))
+console.log(
+  bigToSmall([
+    [1, 3, 5],
+    [2, 4, 6],
+  ]),
+  '6>5>4>3>2>1',
+)
+console.log(bigToSmall([[1, 1], [1], [1, 1]]), '1>1>1>1>1')
+console.log(
+  bigToSmall([
+    [1, 2],
+    [3, 4],
+    [5, 6],
+  ]),
+  '6>5>4>3>2>1',
+)
