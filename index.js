4423567a9d70de1ebda930fe2a2a05666758ddd2
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-08-13 03:35:50 +0300

    some stuff

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-function foldArray(array, runs) {
-    var result = array;
-    for (var i = 0; i < runs; i++) {
-        var foldingPoint = result.length / 2;
-        var firstLoop = [];
-        for (var x = 0; x < Math.round(foldingPoint); x++) {
-            firstLoop.push(result.length % 2
-                ? result.length - x > Math.round(foldingPoint)
-                    ? result[x] + result[result.length - x - 1]
-                    : result[x]
-                : result[x] + result[result.length - x - 1]);
-        }
-        result = firstLoop;
-    }
-    return result;
+function miniMaxSum(arr = []) {
+  // Write your code here
+  const res = arr.map((num, index, array) => {
+    console.log(
+      array.reduce((acc, numb, i) => (index !== i ? (acc += numb) : acc), 0)
+    );
+    return array.reduce((acc, numb) => (acc += numb), 0);
+  });
+  console.log(res);
+  console.log(Math.min(...res));
+  console.log(Math.max(...res));
 }
-var input = [1, 2, 3, 4, 5];
-console.log(foldArray(input, 1), "[ 6, 6, 3 ]");
-console.log(foldArray(input, 2), "[ 9, 6 ]");
-console.log(foldArray(input, 3), "[ 15 ]");
+console.log(miniMaxSum([1, 2, 3, 4, 5]));
--- index.ts
+++ index.ts
@@ -1,24 +1,14 @@
-function foldArray(array: number[], runs: number): number[] {
-  let result: number[] = array;
-  for (let i = 0; i < runs; i++) {
-    const foldingPoint = result.length / 2;
-    let firstLoop = [];
-    for (let x = 0; x < Math.round(foldingPoint); x++) {
-      firstLoop.push(
-        result.length % 2
-          ? result.length - x > Math.round(foldingPoint)
-            ? result[x] + result[result.length - x - 1]
-            : result[x]
-          : result[x] + result[result.length - x - 1]
-      );
+function foldArray(start: number, end: number): number {
+  const arr = [];
+  for (let i = start; i <= end; i++) {
+    let item = `${i}`;
+    if (!item.match(/[5]/)) {
+      arr.push(`${i}`);
     }
-    result = firstLoop;
   }
-  return result;
+  return arr.length;
 }
 
-const input = [1, 2, 3, 4, 5];
-
-console.log(foldArray(input, 1), "[ 6, 6, 3 ]");
-console.log(foldArray(input, 2), "[ 9, 6 ]");
-console.log(foldArray(input, 3), "[ 15 ]");
+console.log(foldArray(4, 17), "12");
+console.log(foldArray(-47, 48), "86");
+console.log(foldArray(-10, 55), "[ 15 ]");
