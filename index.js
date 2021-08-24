0837be3772833d4d06803ae5326e15d8b2441497
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-08-24 14:30:24 +0300

    Fibonacci Calculator Memoized

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-function moveZeros(arr = '') {
-  function twistIt(str = '') {
-    return str.split('').map((a, i, arr) => arr[arr.length - i - 1]).join('')
-  }
-  return arr.split(' ').map(item => twistIt(item)).join(' ')
+function feb(num, memo = {}) {
+  if (num <= 2) return 1;
+  if (num in memo) return memo[num];
+  memo[num] = feb(num - 1, memo) + feb(num - 2, memo);
+  return memo[num];
 }
 
-console.log(moveZeros("double  spaces"), "double  spaces", '==>', "elbuod  secaps")
-console.log(moveZeros("double  spaces"), "double  spaces", '==>', "elbuod  secaps")
-console.log(moveZeros("double  spaces"), "double  spaces", '==>', "elbuod  secaps")
-
-function manhattanDistance(arr0, arr1) {
-  function fn(params) {
-
-    return Math.abs(params)
-  }
-  return (fn(arr0[0] - arr1[0]) + fn(arr0[1] - arr1[1]))
-}
-
-console.log(manhattanDistance([1, 1], [1, 1]), '=> returns 0')
-console.log(manhattanDistance([5, 4], [3, 2]), '=> returns 4')
-console.log(manhattanDistance([1, 1], [0, 3]), '=> returns 3')
+console.log(feb(1));
+console.log(feb(2));
+console.log(feb(3));
+console.log(feb(9));
+console.log(feb(39));
