3cca900051bd16c756d6aa6361b205080dcc8f80
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-08-24 14:55:04 +0300

    Grid Traveler Memod

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-function feb(num, memo = {}) {
-  if (num <= 2) return 1;
-  if (num in memo) return memo[num];
-  memo[num] = feb(num - 1, memo) + feb(num - 2, memo);
-  return memo[num];
+function gridTraveler(row, column, memo = {}) {
+  if (row === 1 && column === 1) return 1;
+  if (row === 0 || column === 0) return 0;
+  const key = row + "," + column;
+  if (key in memo) return memo[key];
+  memo[key] =
+    gridTraveler(row - 1, column, memo) + gridTraveler(row, column - 1, memo);
+  return memo[key];
 }
 
-console.log(feb(1));
-console.log(feb(2));
-console.log(feb(3));
-console.log(feb(9));
-console.log(feb(39));
+console.log(gridTraveler(1, 0));
+console.log(gridTraveler(2, 1));
+console.log(gridTraveler(3, 2));
+console.log(gridTraveler(3, 3));
+console.log(gridTraveler(9, 3));
+console.log(gridTraveler(18, 18));
+console.log(gridTraveler(39, 14));
