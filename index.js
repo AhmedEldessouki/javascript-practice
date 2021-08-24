fa839a167202c058108aacb01b1837fedbe3a0d2
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-08-24 19:40:12 +0300

    How Some

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-function canSum(totalSum, numArr, memo = {}) {
+function howSum(totalSum, numArr, memo = {}) {
   if (totalSum in memo) return memo[totalSum];
-  if (totalSum === 0) return true;
-  if (totalSum < 0) return false;
+  if (totalSum === 0) return [];
+  if (totalSum < 0) return null;
 
   for (let num of numArr) {
     const remainder = totalSum - num;
-    if (canSum(remainder, numArr, memo)) {
-      memo[totalSum] = true;
-      return true;
+    const returnedValue = howSum(remainder, numArr, memo);
+    if (returnedValue) {
+      memo[totalSum] = [...returnedValue, num];
+      return memo[totalSum];
     }
   }
 
-  memo[totalSum] = false;
-  return false;
+  memo[totalSum] = null;
+  return null;
 }
 
-console.log(canSum(1, [9, 2]));
-console.log(canSum(2, [1, 4, 5, 9, 2]));
-console.log(canSum(3, [2, 4, 5, 9, 2]));
-console.log(canSum(3, [1, 4, 5, 9, 2]));
-console.log(canSum(9, [3, 4, 5, 2]));
-console.log(canSum(18, [4, 5, 9, 2]));
-console.log(canSum(300, [7, 14]));
+console.log(howSum(1, [9, 2]));
+console.log(howSum(2, [1, 4, 5, 9, 2]));
+console.log(howSum(3, [2, 4, 5, 9, 2]));
+console.log(howSum(3, [1, 4, 5, 9, 2]));
+console.log(howSum(9, [3, 4, 5, 2]));
+console.log(howSum(18, [4, 5, 9, 2]));
+console.log(howSum(300, [7, 14]));
