1a9d65a7aedf64b669100666e6b8c98cb254f601
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-09-02 19:43:17 +0300

    14/15 street fight part 2 kata

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function getPicks(fighters, position, moves) {
-    // your code
+"use strict";
+exports.__esModule = true;
+exports.superStreetFighterSelection = void 0;
+function superStreetFighterSelection(fighters, position, moves) {
     var hoveredArr = [];
+    console.log(fighters, position, moves);
     for (var _i = 0, moves_1 = moves; _i < moves_1.length; _i++) {
         var move = moves_1[_i];
         switch (move) {
@@ -8,11 +11,27 @@ function getPicks(fighters, position, moves) {
                 if (position[0] !== 0) {
                     position[0] -= 1;
                 }
+                if (fighters[position[0]][position[1]] === "") {
+                    if (position[0] !== 0) {
+                        position[0] = 0;
+                    }
+                    else {
+                        position[0] += 1;
+                    }
+                }
                 hoveredArr.push(fighters[position[0]][position[1]]);
                 break;
             case "down":
-                if (position[0] !== 1) {
+                if (position[0] !== fighters.length - 1) {
                     position[0] += 1;
+                }
+                if (fighters[position[0]][position[1]] === "") {
+                    if (position[0] !== fighters.length - 1) {
+                        position[0] = 0;
+                    }
+                    else {
+                        position[0] += 1;
+                    }
                 }
                 hoveredArr.push(fighters[position[0]][position[1]]);
                 break;
@@ -23,6 +42,17 @@ function getPicks(fighters, position, moves) {
                 else {
                     position[1] -= 1;
                 }
+                if (fighters[position[0]][position[1]] === "") {
+                    if (position[1] === 0) {
+                        position[1] = fighters[0].length - 1;
+                    }
+                    else {
+                        position[1] -= 1;
+                    }
+                    if (fighters[position[0]][position[1]] === "") {
+                        position[1] -= 1;
+                    }
+                }
                 hoveredArr.push(fighters[position[0]][position[1]]);
                 break;
             case "right":
@@ -32,6 +62,17 @@ function getPicks(fighters, position, moves) {
                 else {
                     position[1] += 1;
                 }
+                if (fighters[position[0]][position[1]] === "") {
+                    if (position[1] === fighters[0].length - 1) {
+                        position[1] = 0;
+                    }
+                    else {
+                        position[1] += 1;
+                    }
+                    if (fighters[position[0]][position[1]] === "") {
+                        position[1] += 1;
+                    }
+                }
                 hoveredArr.push(fighters[position[0]][position[1]]);
                 break;
             default:
@@ -40,31 +81,123 @@ function getPicks(fighters, position, moves) {
     }
     return hoveredArr;
 }
+exports.superStreetFighterSelection = superStreetFighterSelection;
 var fighters = [
-    ["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
-    ["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"],
+    ["", "Ryu", "E.Honda", "Blanka", "Guile", ""],
+    ["Balrog", "Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat"],
+    ["Vega", "T.Hawk", "Fei Long", "Deejay", "Cammy", "M.Bison"],
 ];
-var initial_position = [0, 0];
-var moves = ["up", "left", "right", "left", "left"];
-console.log(getPicks(fighters, initial_position, moves), [
-    "Ryu",
-    "Vega",
-    "Ryu",
-    "Vega",
-    "Balrog",
-]);
+var initial_position = [1, 0];
+var moves = ["down", "down", "down", "down"];
+// console.log(superStreetFighterSelection(fighters, initial_position, moves), [
+//   "Vega",
+//   "Vega",
+//   "Vega",
+//   "Vega",
+// ]);
+// fighters = [
+//   ["", "Ryu", "E.Honda", "Blanka", "Guile", ""],
+//   ["Balrog", "Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat"],
+//   ["Vega", "T.Hawk", "Fei Long", "Deejay", "Cammy", "M.Bison"],
+// ];
+// initial_position = [1, 0];
+// moves = ["up"];
+// console.log(superStreetFighterSelection(fighters, initial_position, moves), [
+//   "Balrog",
+// ]);
+// fighters = [
+//   ["", "Ryu", "E.Honda", "Cammy"],
+//   ["Balrog", "Ken", "Chun Li", ""],
+//   ["Vega", "", "Fei Long", "Balrog"],
+//   ["Blanka", "Guile", "", "Chun Li"],
+//   ["M.Bison", "Zangief", "Dhalsim", "Sagat"],
+//   ["Deejay", "Cammy", "", "T.Hawk"],
+// ];
+// initial_position = [0, 3];
+// moves = [
+//   "left",
+//   "left",
+//   "down",
+//   "right",
+//   "right",
+//   "right",
+//   "right",
+//   "down",
+//   "left",
+//   "left",
+//   "left",
+//   "left",
+//   "down",
+//   "right",
+//   "right",
+//   "down",
+//   "right",
+//   "right",
+//   "right",
+//   "down",
+//   "left",
+//   "left",
+//   "left",
+//   "down",
+//   "left",
+//   "left",
+//   "left",
+// ];
+// console.log(superStreetFighterSelection(fighters, initial_position, moves), []);
+// fighters = [
+//   ["", "Ryu", "E.Honda", "Cammy", "Blanka", "Guile", "", "Chun Li"],
+//   ["Balrog", "Ken", "Chun Li", "", "M.Bison", "Zangief", "Dhalsim", "Sagat"],
+//   ["Vega", "", "Fei Long", "Balrog", "Deejay", "Cammy", "", "T.Hawk"],
+// ];
+// initial_position = [0, 3];
+// moves = [
+//   "down",
+//   "right",
+//   "right",
+//   "right",
+//   "down",
+//   "left",
+//   "left",
+//   "down",
+//   "right",
+//   "right",
+//   "right",
+//   "up",
+// ];
+// console.log(superStreetFighterSelection(fighters, initial_position, moves), []);
 fighters = [
-    ["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
-    ["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"],
+    ["", "Ryu", "E.Honda", "Cammy"],
+    ["Balrog", "Ken", "Chun Li", ""],
+    ["Vega", "", "Fei Long", "Balrog"],
+    ["Blanka", "Guile", "", "Chun Li"],
+    ["M.Bison", "Zangief", "Dhalsim", "Sagat"],
+    ["Deejay", "Cammy", "", "T.Hawk"],
 ];
-initial_position = [0, 0];
-moves = ["right", "down", "left", "left", "left", "left", "right"];
-console.log(getPicks(fighters, initial_position, moves), [
-    "E.Honda",
-    "Chun Li",
-    "Ken",
-    "M.Bison",
-    "Sagat",
-    "Dhalsim",
-    "Sagat",
-]);
+initial_position = [3, 1];
+moves = [
+    "up",
+    "left",
+    "left",
+    "down",
+    "right",
+    "right",
+    "up",
+    "right",
+    "right",
+    "left",
+    "left",
+    "down",
+    "up",
+    "down",
+    "up",
+    "up",
+    "down",
+    "left",
+    "left",
+    "left",
+    "left",
+    "up",
+    "right",
+    "left",
+];
+console.log(superStreetFighterSelection(fighters, initial_position, moves), []);
--- index.ts
+++ index.ts
@@ -1,21 +1,37 @@
-function getPicks(
-  fighters: Array<Array<string>>,
-  position: [number, number],
+export function superStreetFighterSelection(
+  fighters: Array<string[]>,
+  position: number[],
   moves: string[]
-): string[] {
-  // your code
+) {
   const hoveredArr = [];
+  console.log(fighters, position, moves);
   for (const move of moves) {
     switch (move) {
       case "up":
         if (position[0] !== 0) {
           position[0] -= 1;
         }
+        if (fighters[position[0]][position[1]] === "") {
+          if (position[0] !== 0) {
+            position[0] -= 1;
+          } else {
+            position[0] += 1;
+          }
+        }
         hoveredArr.push(fighters[position[0]][position[1]]);
         break;
       case "down":
-        if (position[0] !== 1) {
+        if (position[0] !== fighters.length - 1) {
           position[0] += 1;
+        } else {
+          position[0] += 1;
+        }
+        if (fighters[position[0]][position[1]] === "") {
+          if (position[0] !== fighters.length - 1) {
+            position[0] += 1;
+          } else {
+            position[0] -= 1;
+          }
         }
         hoveredArr.push(fighters[position[0]][position[1]]);
         break;
@@ -25,6 +41,16 @@ function getPicks(
         } else {
           position[1] -= 1;
         }
+        if (fighters[position[0]][position[1]] === "") {
+          if (position[1] === 0) {
+            position[1] = fighters[0].length - 1;
+          } else {
+            position[1] -= 1;
+          }
+          if (fighters[position[0]][position[1]] === "") {
+            position[1] -= 1;
+          }
+        }
         hoveredArr.push(fighters[position[0]][position[1]]);
         break;
       case "right":
@@ -33,6 +59,16 @@ function getPicks(
         } else {
           position[1] += 1;
         }
+        if (fighters[position[0]][position[1]] === "") {
+          if (position[1] === fighters[0].length - 1) {
+            position[1] = 0;
+          } else {
+            position[1] += 1;
+          }
+          if (fighters[position[0]][position[1]] === "") {
+            position[1] += 1;
+          }
+        }
         hoveredArr.push(fighters[position[0]][position[1]]);
         break;
 
@@ -40,33 +76,127 @@ function getPicks(
         break;
     }
   }
+
   return hoveredArr;
 }
 let fighters = [
-  ["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
-  ["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"],
+  ["", "Ryu", "E.Honda", "Blanka", "Guile", ""],
+  ["Balrog", "Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat"],
+  ["Vega", "T.Hawk", "Fei Long", "Deejay", "Cammy", "M.Bison"],
 ];
-let initial_position: [number, number] = [0, 0];
-let moves = ["up", "left", "right", "left", "left"];
-console.log(getPicks(fighters, initial_position, moves), [
-  "Ryu",
-  "Vega",
-  "Ryu",
-  "Vega",
-  "Balrog",
-]);
+let initial_position: [number, number] = [1, 0];
+let moves = ["down", "down", "down", "down"];
+// console.log(superStreetFighterSelection(fighters, initial_position, moves), [
+//   "Vega",
+//   "Vega",
+//   "Vega",
+//   "Vega",
+// ]);
+
+// fighters = [
+//   ["", "Ryu", "E.Honda", "Blanka", "Guile", ""],
+//   ["Balrog", "Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat"],
+//   ["Vega", "T.Hawk", "Fei Long", "Deejay", "Cammy", "M.Bison"],
+// ];
+// initial_position = [1, 0];
+// moves = ["up"];
+// console.log(superStreetFighterSelection(fighters, initial_position, moves), [
+//   "Balrog",
+// ]);
+
+// fighters = [
+//   ["", "Ryu", "E.Honda", "Cammy"],
+//   ["Balrog", "Ken", "Chun Li", ""],
+//   ["Vega", "", "Fei Long", "Balrog"],
+//   ["Blanka", "Guile", "", "Chun Li"],
+//   ["M.Bison", "Zangief", "Dhalsim", "Sagat"],
+//   ["Deejay", "Cammy", "", "T.Hawk"],
+// ];
+// initial_position = [0, 3];
+// moves = [
+//   "left",
+//   "left",
+//   "down",
+//   "right",
+//   "right",
+//   "right",
+//   "right",
+//   "down",
+//   "left",
+//   "left",
+//   "left",
+//   "left",
+//   "down",
+//   "right",
+//   "right",
+//   "down",
+//   "right",
+//   "right",
+//   "right",
+//   "down",
+//   "left",
+//   "left",
+//   "left",
+//   "down",
+//   "left",
+//   "left",
+//   "left",
+// ];
+// console.log(superStreetFighterSelection(fighters, initial_position, moves), []);
+// fighters = [
+//   ["", "Ryu", "E.Honda", "Cammy", "Blanka", "Guile", "", "Chun Li"],
+//   ["Balrog", "Ken", "Chun Li", "", "M.Bison", "Zangief", "Dhalsim", "Sagat"],
+//   ["Vega", "", "Fei Long", "Balrog", "Deejay", "Cammy", "", "T.Hawk"],
+// ];
+// initial_position = [0, 3];
+// moves = [
+//   "down",
+//   "right",
+//   "right",
+//   "right",
+//   "down",
+//   "left",
+//   "left",
+//   "down",
+//   "right",
+//   "right",
+//   "right",
+//   "up",
+// ];
+// console.log(superStreetFighterSelection(fighters, initial_position, moves), []);
 fighters = [
-  ["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
-  ["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"],
+  ["", "Ryu", "E.Honda", "Cammy"],
+  ["Balrog", "Ken", "Chun Li", ""],
+  ["Vega", "", "Fei Long", "Balrog"],
+  ["Blanka", "Guile", "", "Chun Li"],
+  ["M.Bison", "Zangief", "Dhalsim", "Sagat"],
+  ["Deejay", "Cammy", "", "T.Hawk"],
 ];
-initial_position = [0, 0];
-moves = ["right", "down", "left", "left", "left", "left", "right"];
-console.log(getPicks(fighters, initial_position, moves), [
-  "E.Honda",
-  "Chun Li",
-  "Ken",
-  "M.Bison",
-  "Sagat",
-  "Dhalsim",
-  "Sagat",
-]);
+initial_position = [3, 1];
+moves = [
+  "up",
+  "left",
+  "left",
+  "down",
+  "right",
+  "right",
+  "up",
+  "right",
+  "right",
+  "left",
+  "left",
+  "down",
+  "up",
+  "down",
+  "up",
+  "up",
+  "down",
+  "left",
+  "left",
+  "left",
+  "left",
+  "up",
+  "right",
+  "left",
+];
+console.log(superStreetFighterSelection(fighters, initial_position, moves), []);
