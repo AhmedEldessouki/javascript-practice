838973218cf6e00418169b5916a6cca621c708a8
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-09-29 19:20:01 +0300

    CodeWars: Using closures to share class state

--- index.js
+++ index.js
@@ -1,65 +1,45 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-function solve(n, k) {
-    // Write your code here
-    if (n === 1 && k !== 1)
-        return [-1];
-    if (n === 1 && k === 1)
-        return [1];
-    var max = 0;
-    var ar = [];
-    var arr = [];
-    for (var i = 1; i <= n; i++) {
-        ar.push(i);
-    }
-    var memo = {};
-    for (var x = 0; x < ar.length; x++) {
-        var el = Math.round(ar.length / 2);
-        for (var y = 0; y < ar.length; y++) {
-            var key = ar.join(",");
-            if (!(key in memo)) {
-                memo[key] = Math.min.apply(Math, ar.reduce(function (acc, num, z) {
-                    if (z + 1 < ar.length)
-                        acc.push(Math.abs(num - ar[z + 1]));
-                    return acc;
-                }, []));
-                console.log(ar, memo[key]);
-                if (memo[key] > max) {
-                    arr = [];
-                    console.log(" < minimum");
-                    max = memo[key];
-                    arr.push(__spreadArray([], ar, true));
-                }
-                else if (memo[key] === max) {
-                    max = memo[key];
-                    console.log("mo[key");
-                    arr.push(__spreadArray([], ar, true));
-                }
-            }
-            y === ar.length - 2
-                ? ar.push(ar.splice(ar.length - 2, 1)[0])
-                : ar.unshift(ar.splice(ar.length - 1, 1)[0]);
-        }
-    }
-    console.log(arr);
-    return arr[Math.abs(k - arr.length)] || arr.pop();
-}
-console.log(solve(3, 5), "3 1 2");
-console.log(solve(4, 2), "3 1 4 2");
-console.log(solve(1, 1));
-console.log(solve(1, 3));
-console.log(solve(1, 5));
-console.log(solve(1, 7));
-console.log(solve(1, 6));
-console.log(solve(1, 2));
-console.log(solve(1, 9));
-console.log(solve(1, 8));
-console.log(solve(1, 10));
-console.log(solve(1, 4));
+// Let's make a Cat constructor!
+var Cat = (function () {
+  // ... your code here.
+
+  Cat.arr = [];
+  function Cat(name, weight) {
+    if (!name || !weight) throw new Error("");
+    this.name = name;
+    this._weight = weight;
+    Object.defineProperty(this, "weight", {
+      enumerable: true,
+      configurable: false,
+      get: function () {
+        return this._weight;
+      },
+      set: function (v) {
+        this._weight = v;
+      },
+    });
+    Cat.arr.push(this);
+  }
+  Cat.averageWeight = function () {
+    return (
+      Cat.arr.reduce(
+        (acc, instance) => (instance.weight ? (acc += instance.weight) : acc),
+        0
+      ) / Cat.arr.length
+    );
+  };
+
+  return Cat;
+})();
+
+const fluffy = new Cat("fluffy", 15);
+const garfield = new Cat("garfield", 25);
+
+console.log(fluffy.weight, 15);
+console.log(fluffy instanceof Cat, true);
+// @ts-ignore
+console.log(fluffy.averageWeight, undefined);
+console.log(typeof Cat.averageWeight, "function");
+fluffy.weight = 200;
+console.log(fluffy.weight, 20);
+console.log(garfield.weight);
+console.log(Cat.averageWeight(), 20);
