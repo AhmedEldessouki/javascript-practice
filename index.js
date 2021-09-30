3019cdcf347b51d85aeafc18f48bd6ca513f05e0
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-10-01 02:51:55 +0300

    CodeWars: Connect 4

--- index.js
+++ index.js
@@ -1,45 +1,155 @@
-// Let's make a Cat constructor!
-var Cat = (function () {
-  // ... your code here.
-
-  Cat.arr = [];
-  function Cat(name, weight) {
-    if (!name || !weight) throw new Error("");
-    this.name = name;
-    this._weight = weight;
-    Object.defineProperty(this, "weight", {
-      enumerable: true,
-      configurable: false,
-      get: function () {
-        return this._weight;
-      },
-      set: function (v) {
-        this._weight = v;
-      },
-    });
-    Cat.arr.push(this);
-  }
-  Cat.averageWeight = function () {
-    return (
-      Cat.arr.reduce(
-        (acc, instance) => (instance.weight ? (acc += instance.weight) : acc),
-        0
-      ) / Cat.arr.length
-    );
-  };
-
-  return Cat;
-})();
-
-const fluffy = new Cat("fluffy", 15);
-const garfield = new Cat("garfield", 25);
-
-console.log(fluffy.weight, 15);
-console.log(fluffy instanceof Cat, true);
-// @ts-ignore
-console.log(fluffy.averageWeight, undefined);
-console.log(typeof Cat.averageWeight, "function");
-fluffy.weight = 200;
-console.log(fluffy.weight, 20);
-console.log(garfield.weight);
-console.log(Cat.averageWeight(), 20);
+function check(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i][0] && arr[i].length > 3) {
+            for (var x = 0; x < arr[i].length - 3; x++) {
+                if (arr[i][x] !== "" &&
+                    arr[i][x] === arr[i][x + 1] &&
+                    arr[i][x + 1] === arr[i][x + 2] &&
+                    arr[i][x + 2] === arr[i][x + 3]) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+function Connect4() {
+    this.isWinner = true;
+    // ? (1*7)*6
+    this.vertical = [];
+    // ? (1*6)*7
+    this.horizontal = [];
+    // ? (1*!)*12
+    this.diagonal = [];
+    this.player = 2;
+    this.movementMemo = {};
+    this.turn = 0;
+    if (this.isWinner) {
+        this.vertical = [];
+        for (var i = 0; i < 7; i++) {
+            this.vertical.push(["", "", "", "", "", ""]);
+            if (i < 6) {
+                this.horizontal.push(["", "", "", "", "", "", ""]);
+                this.diagonal.push([]);
+                this.diagonal.push([]);
+            }
+        }
+        this.isWinner = false;
+    }
+    this.verify = function () {
+        this.isWinner = check(this.horizontal);
+        if (this.isWinner)
+            return;
+        this.isWinner = check(this.vertical);
+        if (this.isWinner)
+            return;
+        if (this.turn < 8)
+            return;
+        console.log("check", this.diagonal, this.vertical, this.horizontal);
+        this.isWinner = check(this.diagonal);
+        if (this.isWinner)
+            return;
+    };
+}
+Connect4.prototype.play = function (col) {
+    // Code Here
+    if (this.isWinner)
+        return "Game has finished!";
+    if (this.movementMemo[col] === 6)
+        return "Column full!";
+    this.player = this.player === 1 ? 2 : 1;
+    if (!(col in Object(this.movementMemo))) {
+        this.movementMemo[col] = 0;
+    }
+    var difference = col - this.movementMemo[col];
+    if (-3 < difference && difference < 4) {
+        if (difference < 0) {
+            difference = this.diagonal.length - 1 + difference;
+        }
+        this.diagonal[difference].push("" + this.player);
+    }
+    difference = col + this.movementMemo[col];
+    if (2 < difference && difference < 9) {
+        this.diagonal[4 + difference].push(this.player);
+    }
+    this.vertical[col][this.movementMemo[col]] += this.player;
+    this.horizontal[this.movementMemo[col]][col] += this.player;
+    this.movementMemo[col] += 1;
+    this.turn += 1;
+    if (!this.isWinner && this.turn > 6) {
+        this.verify();
+    }
+    if (this.isWinner)
+        return "Player " + this.player + " wins!";
+    return "Player " + this.player + " has a turn";
+};
+var game = new Connect4();
+console.log(game.play(3), "Player 1 has a turn");
+console.log(game.play(3), "Player 2 has a turn");
+console.log(game.play(2), "Player 1 has a turn");
+console.log(game.play(0), "Player 2 has a turn");
+console.log(game.play(2), "Player 1 has a turn");
+console.log(game.play(2), "Player 2 has a turn");
+console.log(game.play(0), "Player 1 has a turn");
+console.log(game.play(1), "Player 2 has a turn");
+console.log(game.play(1), "Player 1 has a turn");
+console.log(game.play(1), "Player 2 has a turn");
+console.log(game.play(0), "Player 1 has a turn");
+console.log(game.play(1), "Player 2 has a turn");
+console.log(game.play(0), "Player 1 has a turn");
+console.log(game.play(0), "Player 2 wins!");
+// game = new Connect4();
+// console.log(game.play(0), "Player 1 has a turn");
+// console.log(game.play(1), "Player 2 has a turn");
+// console.log(game.play(0), "Player 1 has a turn");
+// console.log(game.play(1), "Player 2 has a turn");
+// console.log(game.play(0), "Player 1 has a turn");
+// console.log(game.play(1), "Player 2 has a turn");
+// console.log(game.play(0), "Player 1 wins!");
+// game = new Connect4();
+// console.log(game.play(0), "Player 1 has a turn");
+// console.log(game.play(0), "Player 1 has a turn");
+// console.log(game.play(0), "Player 1 has a turn");
+// console.log(game.play(0), "Player 1 has a turn");
+// console.log(game.play(0), "Player 1 has a turn");
+// console.log(game.play(0), "Player 1 has a turn");
+// console.log(game.play(1), "Player 1 has a turn");
+// console.log(game.play(1), "Player 1 has a turn");
+// console.log(game.play(1), "Player 1 has a turn");
+// console.log(game.play(1), "Player 1 has a turn");
+// console.log(game.play(1), "Player 1 has a turn");
+// console.log(game.play(2), "Player 2 has a turn");
+// console.log(game.play(2), "Player 2 has a turn");
+// console.log(game.play(2), "Player 2 has a turn");
+// console.log(game.play(2), "Player 1 has a turn");
+// console.log(game.play(2), "Player 1 has a turn");
+// console.log(game.play(3), "Player 2 has a turn");
+// console.log(game.play(3), "Player 1 has a turn");
+// console.log(game.play(3), "Player 2 has a turn");
+// console.log(game.play(3), "Player 2 has a turn");
+// console.log(game.play(4), "Player 1 has a turn");
+// console.log(game.play(4), "Player 1 has a turn");
+// console.log(game.play(4), "Player 2 has a turn");
+// console.log(game.play(4), "Player 1 has a turn");
+// console.log(game.play(4), "Player 2 has a turn");
+// console.log(game.play(5), "Player 1 has a turn");
+// console.log(game.play(5), "Player 2 has a turn");
+// console.log(game.play(5), "Player 1 has a turn");
+// console.log(game.play(5), "Player 2 has a turn");
+// console.log(game.play(5), "Player 1 has a turn");
+// console.log(game.play(5), "Player 1 has a turn");
+// console.log(game.play(6), "Player 2 has a turn");
+// console.log(game.play(6), "Player 1 has a turn");
+// console.log(game.play(6), "Player 2 has a turn");
+// console.log(game.play(6), "Column full!");
+// console.log(game.play(6), "Column full!");
+// console.log(game.play(6), "Column full!");
+// game = new Connect4();
+// console.log(game.play(1), "Player 1 has a turn");
+// console.log(game.play(1), "Player 2 has a turn");
+// console.log(game.play(2), "Player 1 has a turn");
+// console.log(game.play(2), "Player 2 has a turn");
+// console.log(game.play(3), "Player 1 has a turn");
+// console.log(game.play(3), "Player 2 has a turn");
+// console.log(game.play(4), "Player 1 wins!");
+// console.log(game.play(4), "Game has finished!");
--- index.ts
+++ index.ts
@@ -1,54 +1,176 @@
-function solve(n: number, k: number): number[] {
-  // Write your code here
-  if (n === 1 && k !== 1) return [-1];
-  if (n === 1 && k === 1) return [1];
-  let max = 0;
-  const ar: number[] = [];
-  let arr: number[][] = [];
-  for (let i = 1; i <= n; i++) {
-    ar.push(i);
-  }
-
-  const memo: { [key: string]: number } = {};
-
-  for (let x = 0; x < ar.length; x++) {
-    const el = Math.round(ar.length / 2);
-    for (let y = 0; y < ar.length; y++) {
-      const key = ar.join(",");
-      if (!(key in memo)) {
-        memo[key] = Math.min(
-          ...ar.reduce((acc: number[], num, z) => {
-            if (z + 1 < ar.length) acc.push(Math.abs(num - ar[z + 1]));
-            return acc;
-          }, [])
-        );
-        if (memo[key] > max) {
-          arr = [];
-          max = memo[key];
-          arr.push([...ar]);
-        } else if (memo[key] === max) {
-          max = memo[key];
-          arr.push([...ar]);
+function check(arr: Array<Array<string>>): boolean {
+  for (let i = 0; i < arr.length; i++) {
+    if (0 in arr[i] && arr[i].length > 3) {
+      for (let x = 0; x < arr[i].length - 3; x++) {
+        if (
+          arr[i][x] !== "" &&
+          arr[i][x] === arr[i][x + 1] &&
+          arr[i][x + 1] === arr[i][x + 2] &&
+          arr[i][x + 2] === arr[i][x + 3]
+        ) {
+          return true;
         }
       }
-      y === ar.length - 2
-        ? ar.push(ar.splice(ar.length - 2, 1)[0])
-        : ar.unshift(ar.splice(ar.length - 1, 1)[0]);
     }
   }
-  return arr[Math.abs(k - arr.length)] || arr.pop();
+  return false;
 }
 
-console.log(solve(3, 5), `3 1 2`);
-console.log(solve(4, 2), `3 1 4 2`);
+function Connect4(this: {
+  vertical: Array<Array<string>>;
+  horizontal: Array<Array<string>>;
+  diagonal: Array<Array<string>>;
+  isWinner: boolean;
+  player: 1 | 2;
+  movementMemo: { [key: number]: number };
+  turn: number;
+  verify: () => void;
+}) {
+  this.isWinner = true;
+  // ? (1*7)*6
+  this.vertical = [];
+  // ? (1*6)*7
+  this.horizontal = [];
+  // ? (1*!)*12
+  this.diagonal = [];
+  this.player = 2;
+  this.movementMemo = {};
+  this.turn = 0;
 
-console.log(solve(1, 1));
-console.log(solve(1, 3));
-console.log(solve(1, 5));
-console.log(solve(1, 7));
-console.log(solve(1, 6));
-console.log(solve(1, 2));
-console.log(solve(1, 9));
-console.log(solve(1, 8));
-console.log(solve(1, 10));
-console.log(solve(1, 4));
+  if (this.isWinner) {
+    this.vertical = [];
+    for (let i = 0; i < 7; i++) {
+      this.vertical.push(["", "", "", "", "", ""]);
+      if (i < 6) {
+        this.horizontal.push(["", "", "", "", "", "", ""]);
+        this.diagonal.push([]);
+        this.diagonal.push([]);
+      }
+    }
+    this.isWinner = false;
+  }
+  this.verify = function () {
+    this.isWinner = check(this.horizontal);
+    if (this.isWinner) return;
+    this.isWinner = check(this.vertical);
+    if (this.isWinner) return;
+
+    if (this.turn < 8) return;
+    this.isWinner = check(this.diagonal);
+    if (this.isWinner) return;
+  };
+}
+
+Connect4.prototype.play = function (col: number) {
+  // Code Here
+  if (this.isWinner) return "Game has finished!";
+  if (this.movementMemo[col] === 6) return "Column full!";
+
+  this.player = this.player === 1 ? 2 : 1;
+
+  if (!(col in this.movementMemo)) {
+    this.movementMemo[col] = 0;
+  }
+
+  let difference = col - this.movementMemo[col];
+  if (-3 < difference && difference < 4) {
+    if (difference < 0) {
+      difference = this.diagonal.length - 1 + difference;
+    }
+    this.diagonal[difference].push(`${this.player}`);
+  }
+  difference = col + this.movementMemo[col];
+  if (2 < difference && difference < 9) {
+    this.diagonal[3 + difference].push(this.player);
+  }
+
+  this.vertical[col][this.movementMemo[col]] += this.player;
+  this.horizontal[this.movementMemo[col]][col] += this.player;
+
+  this.movementMemo[col] += 1;
+
+  this.turn += 1;
+
+  if (!this.isWinner && this.turn > 6) {
+    this.verify();
+  }
+
+  if (this.isWinner) return `Player ${this.player} wins!`;
+
+  return `Player ${this.player} has a turn`;
+};
+
+let game = new Connect4();
+
+console.log(game.play(3), "Player 1 has a turn");
+console.log(game.play(3), "Player 2 has a turn");
+console.log(game.play(2), "Player 1 has a turn");
+console.log(game.play(0), "Player 2 has a turn");
+console.log(game.play(2), "Player 1 has a turn");
+console.log(game.play(2), "Player 2 has a turn");
+console.log(game.play(0), "Player 1 has a turn");
+console.log(game.play(1), "Player 2 has a turn");
+console.log(game.play(1), "Player 1 has a turn");
+console.log(game.play(1), "Player 2 has a turn");
+console.log(game.play(0), "Player 1 has a turn");
+console.log(game.play(1), "Player 2 has a turn");
+console.log(game.play(0), "Player 1 has a turn");
+console.log(game.play(0), "Player 2 wins!");
+
+// game = new Connect4();
+// console.log(game.play(0), "Player 1 has a turn");
+// console.log(game.play(1), "Player 2 has a turn");
+// console.log(game.play(0), "Player 1 has a turn");
+// console.log(game.play(1), "Player 2 has a turn");
+// console.log(game.play(0), "Player 1 has a turn");
+// console.log(game.play(1), "Player 2 has a turn");
+// console.log(game.play(0), "Player 1 wins!");
+
+// game = new Connect4();
+// console.log(game.play(0), "Player 1 has a turn");
+// console.log(game.play(0), "Player 1 has a turn");
+// console.log(game.play(0), "Player 1 has a turn");
+// console.log(game.play(0), "Player 1 has a turn");
+// console.log(game.play(0), "Player 1 has a turn");
+// console.log(game.play(0), "Player 1 has a turn");
+// console.log(game.play(1), "Player 1 has a turn");
+// console.log(game.play(1), "Player 1 has a turn");
+// console.log(game.play(1), "Player 1 has a turn");
+// console.log(game.play(1), "Player 1 has a turn");
+// console.log(game.play(1), "Player 1 has a turn");
+// console.log(game.play(2), "Player 2 has a turn");
+// console.log(game.play(2), "Player 2 has a turn");
+// console.log(game.play(2), "Player 2 has a turn");
+// console.log(game.play(2), "Player 1 has a turn");
+// console.log(game.play(2), "Player 1 has a turn");
+// console.log(game.play(3), "Player 2 has a turn");
+// console.log(game.play(3), "Player 1 has a turn");
+// console.log(game.play(3), "Player 2 has a turn");
+// console.log(game.play(3), "Player 2 has a turn");
+// console.log(game.play(4), "Player 1 has a turn");
+// console.log(game.play(4), "Player 1 has a turn");
+// console.log(game.play(4), "Player 2 has a turn");
+// console.log(game.play(4), "Player 1 has a turn");
+// console.log(game.play(4), "Player 2 has a turn");
+// console.log(game.play(5), "Player 1 has a turn");
+// console.log(game.play(5), "Player 2 has a turn");
+// console.log(game.play(5), "Player 1 has a turn");
+// console.log(game.play(5), "Player 2 has a turn");
+// console.log(game.play(5), "Player 1 has a turn");
+// console.log(game.play(5), "Player 1 has a turn");
+// console.log(game.play(6), "Player 2 has a turn");
+// console.log(game.play(6), "Player 1 has a turn");
+// console.log(game.play(6), "Player 2 has a turn");
+// console.log(game.play(6), "Column full!");
+// console.log(game.play(6), "Column full!");
+// console.log(game.play(6), "Column full!");
+
+// game = new Connect4();
+// console.log(game.play(1), "Player 1 has a turn");
+// console.log(game.play(1), "Player 2 has a turn");
+// console.log(game.play(2), "Player 1 has a turn");
+// console.log(game.play(2), "Player 2 has a turn");
+// console.log(game.play(3), "Player 1 has a turn");
+// console.log(game.play(3), "Player 2 has a turn");
+// console.log(game.play(4), "Player 1 wins!");
+// console.log(game.play(4), "Game has finished!");
