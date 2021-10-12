35247bf54477e120fa78ce359fd66aa8a05b4481
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-10-12 15:11:58 +0300

    CodeWars: Fibonacci Streaming

--- index.js
+++ index.js
@@ -1,53 +1,14 @@
 "use strict";
 exports.__esModule = true;
-exports.G964 = void 0;
-var G964 = /** @class */ (function () {
-    function G964() {
+exports.fibonacciSequence = void 0;
+function fibonacciSequence() {
+    // To be implemented: Proper implementation.
+    var feb = [1, 1, 2, 3, 5, 8, 13];
+    for (var i = feb.length - 1; feb[i] < Infinity; i++) {
+        feb.push(feb[i] + feb[i - 1]);
     }
-    var _a;
-    _a = G964;
-    G964.decompose = function (n, m) {
-        if (m === void 0) { m = 0; }
-        // your code
-        var goal = Math.pow(n, 2);
-        var x = n - 1;
-        var y = 0;
-        var nums = [];
-        while (goal !== y && x >= 0) {
-            if (goal > y) {
-                y += Math.pow(x, 2);
-                nums.push(x);
-                if (goal < y) {
-                    y -= Math.pow(x, 2);
-                    nums.pop();
-                }
-                if (x === 0 && goal > y) {
-                    if (m !== 0) {
-                        x = nums[0] - 1;
-                        nums = [];
-                        y = 0;
-                    }
-                    else {
-                        x = nums[1] - 1;
-                        nums = nums.splice(0, 1);
-                        y = Math.pow(nums[0], 2);
-                    }
-                }
-                else {
-                    x--;
-                }
-            }
-        }
-        return goal === y
-            ? nums.reverse()
-            : m === 0
-                ? _a.decompose(n, 1)
-                : "Nothing";
-    };
-    return G964;
-}());
-exports.G964 = G964;
-console.log(G964.decompose(4), "Nothing");
-console.log(G964.decompose(12), [1, 2, 3, 7, 9]);
-console.log(G964.decompose(50), [1, 3, 5, 8, 49]);
-console.log(G964.decompose(44), [2, 3, 5, 7, 43]);
+    console.log(feb);
+    return feb[Symbol.iterator]();
+}
+exports.fibonacciSequence = fibonacciSequence;
+console.log(fibonacciSequence());
--- index.ts
+++ index.ts
@@ -1,42 +1,10 @@
-export class G964 {
-  public static decompose = (n: number, m: number = 0): number[] | string => {
-    // your code
-    const goal = Math.pow(n, 2);
-    let x = n - 1;
-    let y = 0;
-    let nums: number[] = [];
-    while (goal !== y && x >= 0) {
-      if (goal > y) {
-        y += Math.pow(x, 2);
-        nums.push(x);
-        if (goal < y) {
-          y -= Math.pow(x, 2);
-          nums.pop();
-        }
-        if (x === 0 && goal > y) {
-          if (m !== 0) {
-            x = nums[0] - 1;
-            nums = [];
-            y = 0;
-          } else {
-            x = nums[1] - 1;
-            nums = nums.splice(0, 1);
-            y = Math.pow(nums[0], 2);
-          }
-        } else {
-          x--;
-        }
-      }
-    }
-    return goal === y
-      ? nums.reverse()
-      : m === 0
-      ? this.decompose(n, 1)
-      : "Nothing";
-  };
+export function fibonacciSequence(): Iterator<number> {
+  // To be implemented: Proper implementation.
+  const feb = [1, 1, 2, 3, 5, 8, 13];
+  for (let i = feb.length - 1; feb[i] < Infinity; i++) {
+    feb.push(feb[i] + feb[i - 1]);
+  }
+  console.log(feb);
+  return feb[Symbol.iterator]();
 }
-
-console.log(G964.decompose(4), "Nothing");
-console.log(G964.decompose(12), [1, 2, 3, 7, 9]);
-console.log(G964.decompose(50), [1, 3, 5, 8, 49]);
-console.log(G964.decompose(44), [2, 3, 5, 7, 43]);
+console.log(fibonacciSequence());
