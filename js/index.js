e303b9446682fe8fd2e9cc9f8570daf753bbff8c
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-10-14 14:03:24 +0300

    CodeWars: Going to zero or to infinity?

--- js/index.js
+++ js/index.js
@@ -5,10 +5,33 @@ class G964 {
     static going(n) {
         const factorialsSum = [0];
         // your code
-        let sum = 1;
+        let str = "";
+        const x = n > 200 ? 200 : n;
+        for (let i = 0; i < x; i++) {
+            str += "0";
+        }
+        let fac = Number("0." + str + "1");
+        let y = 1;
         for (let i = 1; i <= n; i++) {
-            sum *= i;
-            factorialsSum[i] = sum;
+            fac *= i;
+            // if (i === 201) {
+            //   fac = Number("0." + str + "1");
+            // }
+            // if (i === 351) {
+            //   fac = Number("0." + str + "1");
+            // }
+            if (y === 201) {
+                console.log(`-----------------------`);
+                fac = Number("0." + str + "1");
+            }
+            if (y === 351) {
+                console.log(`-----------------------`);
+                y = 1;
+                fac = Number("0." + str + "1");
+            }
+            // console.log(i, fac);
+            factorialsSum[i] = fac;
+            y++;
         }
         const b = factorialsSum.reduce((acc, num) => (acc += num), 0);
         return Number(((1 / factorialsSum[n]) * b)
@@ -26,5 +49,8 @@ exports.G964 = G964;
 console.log(G964.going(5), 1.275);
 console.log(G964.going(6), 1.2125);
 console.log(G964.going(7), 1.173214);
-// ! Infinity Returns NaN
-console.log(G964.going(178), 1.005649);
+console.log(G964.going(200), 1.005025);
+console.log(G964.going(172), 1.005848);
+console.log(G964.going(523), 1.001915);
+//Test : 123 n: 102 --> 1.009901
+console.log(G964.going(1011), 1.00099);
--- ts/index.ts
+++ ts/index.ts
@@ -2,12 +2,36 @@ export class G964 {
   public static going(n: number): number {
     const factorialsSum: number[] = [0];
     // your code
-    let sum = 1;
-    for (let i = 1; i <= n; i++) {
-      sum *= i;
-      factorialsSum[i] = sum;
+    let str = "";
+    const x = n > 200 ? 200 : n;
+    for (let i = 0; i < x; i++) {
+      str += "0";
     }
-    const b = factorialsSum.reduce((acc, num) => (acc += num), 0);
+    let fac = Number("0." + str + "1");
+    let y = 1;
+    for (let i = 1; i <= n; i++) {
+      fac *= i;
+      // if (i === 201) {
+      //   fac = Number("0." + str + "1");
+      // }
+      // if (i === 351) {
+      //   fac = Number("0." + str + "1");
+      // }
+      if (y === 201) {
+        console.log(`-----------------------`);
+        fac = Number("0." + str + "1");
+      }
+      if (y === 351) {
+        console.log(`-----------------------`);
+        y = 1;
+        fac = Number("0." + str + "1");
+      }
+      // console.log(i, fac);
+      factorialsSum[i] = fac;
+      y++;
+    }
+    const b: number = factorialsSum.reduce((acc, num) => (acc += num), 0);
+
     return Number(
       ((1 / factorialsSum[n]) * b)
         .toString()
@@ -24,5 +48,8 @@ export class G964 {
 console.log(G964.going(5), 1.275);
 console.log(G964.going(6), 1.2125);
 console.log(G964.going(7), 1.173214);
-// ! Infinity Returns NaN
-console.log(G964.going(178), 1.005649);
+console.log(G964.going(200), 1.005025);
+console.log(G964.going(172), 1.005848);
+console.log(G964.going(523), 1.001915);
+//Test : 123 n: 102 --> 1.009901
+console.log(G964.going(1011), 1.00099);
