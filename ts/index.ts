9be807ae59cf8963b611bad280431a0cf7691da9
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-11-11 15:55:13 +0300

    CodeWars: Sum of pairs

--- ts/index.ts
+++ ts/index.ts
@@ -1,74 +1,78 @@
-var maxSequence = function (arr: number[]) {
-  // ...
-  if (arr.length <= 0) return 0;
-  if (Math.max(...arr) < 0) return 0;
-
-  function sumUp(ar: number[]) {
-    return ar.reduce((acc, num) => (acc += num), 0);
-  }
-
-  if (Math.min(...arr) >= 0) return sumUp(arr);
-
-  const memo: { [key: string]: number } = {};
-  let x = 1;
-  while (x < arr.length) {
-    for (let i = 0; i < arr.length; i++) {
-      const els = [...arr].splice(i, x);
-      const key = els.join(",");
-      if (!(key in memo)) {
-        memo[key] = sumUp(els);
+export function sumPairs(ints: number[], s: number): [number, number] | void {
+  const memo: { [key: number]: boolean } = {};
+  for (let i = 0; i < ints.length; i++) {
+    if (!(ints[i] in memo)) {
+      memo[ints[i]] = true;
+      for (let x = i + 1; x < ints.length; x++) {
+        const sum = ints[i] + ints[x];
+        const sum2 = ints[x] + ints[x + 1];
+        if (sum2 === s) {
+          return [ints[x], ints[x + 1]];
+        }
+        if (sum === s) {
+          return [ints[i], ints[x]];
+        }
       }
     }
-    x++;
   }
-  const key = arr.join(",");
-  memo[key] = sumUp(arr);
-  // console.log(memo);
-  return Math.max(...Object.values(memo));
-};
-// console.log(maxSequence([]), 0);
-// console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6);
-// console.log(maxSequence([2, 1, 3, 4, 1, 2, 1, 5, 4]), 23);
-// console.log(maxSequence([-2, -1, -3, -4, -1, -2, -1, -5, -4]), 0);
+
+  return; // your code here...
+}
+
+const l1: number[] = [1, 4, 8, 7, 3, 15],
+  l2: number[] = [1, -2, 3, 0, -6, 1],
+  l3: number[] = [20, -13, 40],
+  l4: number[] = [1, 2, 3, 4, 1, 0],
+  l5: number[] = [10, 5, 2, 3, 7, 5],
+  l6: number[] = [4, -2, 3, 3, 4],
+  l7: number[] = [0, 2, 0],
+  l8: number[] = [5, 9, 13, -3],
+  l9: number[] = [10, 5, 2, 3, 7, 5];
+
 console.log(
-  maxSequence([
-    7, 4, 11, -11, 39, 36, 10, -6, 37, -10, -32, 44, -26, -34, 43, 43,
-  ]),
-  155
+  sumPairs(l1, 8),
+  [1, 7],
+  "Basic: [" + l1 + "] should return [1, 7] for sum = 8"
 );
 console.log(
-  maxSequence([
-    -18, -40, -26, -13, -24, 44, 44, -18, -42, -17, -3, -9, 27, 30, 33, -46, 39,
-    -45, 48, 44, 35, -42, 41, 8, 10, 7, -31, -2, 16, 30, 45, 16, -13, 13, 26,
-  ]),
-  289
+  sumPairs(l2, -6),
+  [0, -6],
+  "Negatives: [" + l2 + "] should return [0, -6] for sum = -6"
 );
-console.log(maxSequence([-43, 19, 15, 7, 23, 31]), 95);
 console.log(
-  maxSequence([
-    -24, -46, -18, -29, 50, -19, 40, 48, -46, 36, -16, -25, 3, -20, 32, -6, 29,
-    30, -22, -6, 5, -38, -31, -16, 0, -37, -14, 43, 12, -39, -3, 47, 4, -43, 37,
-    35, 24, 28, -36, -27, 29, 18, 47, -28, 29, 12, -4, -18, 50, -34, -10, 21,
-    15, 13,
-  ]),
-  222
+  sumPairs(l3, -7),
+  undefined,
+  "No Match: [" + l3 + "] should return undefined for sum = -7"
 );
-console.log(maxSequence([-20, 45]), 45);
 console.log(
-  maxSequence([
-    10, 5, 19, 6, 35, 41, -9, 28, 0, 0, -13, -37, -34, 47, 13, 18, -28, 4, -43,
-    -2, 27, -32, 34, -29, 11, 30, 41, -7, 12, -41, 5, -28, 48, 14, -2, 29, -48,
-    48, -5, 18, 37, 43, 30, -27, 2, 32, 30,
-  ]),
-  332
+  sumPairs(l4, 2),
+  [1, 1],
+  "First Match From Left: [" + l4 + "] should return [1, 1] for sum = 2 "
 );
-console.log(maxSequence([-3, 27, 25, -37, 36, 28]), 79);
 console.log(
-  maxSequence([
-    9, 47, 34, -16, 21, -11, 48, 38, -46, 17, 17, 50, 22, 3, 43, -13, 35, -38,
-    50, 6, 1, -44, -6, -39, -26, -42, 45, -5, 20, -37, 43, 30, 7, 17, -43, -22,
-    35, 14, 0, -0, 12, -8, -19, -5, 18, -13, -3, 9, -23, 28, -19, 35, 39, -45,
-    -36, 39, 12, 3, 36, 40, 6, -13,
-  ]),
-  370
+  sumPairs(l5, 10),
+  [3, 7],
+  "First Match From Left REDUX!: [" +
+    l5 +
+    "] should return [3, 7] for sum = 10 "
+);
+console.log(
+  sumPairs(l6, 8),
+  [4, 4],
+  "Duplicates: [" + l6 + "] should return [4, 4] for sum = 8"
+);
+console.log(
+  sumPairs(l7, 0),
+  [0, 0],
+  "Zeroes: [" + l7 + "] should return [0, 0] for sum = 0"
+);
+console.log(
+  sumPairs(l8, 10),
+  [13, -3],
+  "Subtraction: [" + l8 + "] should return [13, -3] for sum = 10"
+);
+console.log(
+  sumPairs(l9, 10),
+  [3, 7],
+  "Subtraction: [" + l9 + "] should return [ 3, 7 ] for sum = 10"
 );
